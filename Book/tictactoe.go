399d73b96d22290e5f6f33dbabd8f8e4413a881c
maddox-bayn <eoduh22@gmail.com>
2025-12-12 14:33:47 +0100

    update repo

---
+++ Book/tictactoe.go
@@ -0,0 +1,29 @@
+//Create a 3×3 tic-tac-toe board using var board [3][3]rune and let the middle be x
+
+package Book
+
+import (
+	"fmt"
+)
+
+func Qain() {
+	fmt.Println("Tic-Tac-Toe Board:")
+
+	var board [3][3]rune
+
+	// Initialize board: place 'X' in the center, spaces elsewhere
+	for i := range board {
+		for j := range board[i] {
+			if i == 1 && j == 1 {
+				board[i][j] = 'X'
+			} else {
+				board[i][j] = ' '
+			}
+		}
+	}
+
+	// Print rows
+	for _, row := range board {
+		fmt.Printf("|%c|%c|%c|\n", row[0], row[1], row[2])
+	}
+}
